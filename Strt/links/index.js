9200bff3587ccc131669caa8653139e8eb104bee
SethPuhala <67097737+SethPuhala@users.noreply.github.com>
2021-07-25 09:47:14 -0700

    Add files via upload

---
+++ Strt/links/index.js
@@ -0,0 +1,23 @@
+let now = new Date();
+let hours = now.getHours();
+if (hours > 12){
+	hours -=12;
+}
+let minutes = now.getMinutes();
+let time = (hours + ':' + minutes)
+document.getElementById("timetext").innerHTML = time
+
+function lightOn() {
+	console.log('on')
+	let reqon = new XMLHttpRequest();
+	reqon.open('POST', "https://maker.ifttt.com/trigger/lights-toggle/with/key/klVzGE694kL9d8sHgn1Ac2FpOtMoHsTTzlfgQZqCAu6", true);
+	reqon.send()
+}
+
+function lightOff() {
+	console.log('off')
+	let reqon = new XMLHttpRequest();
+	reqon.open('POST', "https://maker.ifttt.com/trigger/lights-off/with/key/klVzGE694kL9d8sHgn1Ac2FpOtMoHsTTzlfgQZqCAu6", true);
+	reqon.send()
+}
+
